41a90dcc36c30a09e949d6d857ef5b31b626e8d2
Mike Stickel <mstickel@salesforce.com>
2022-01-24 15:21:50 -0800

    removes unused navSection module

--- src/modules/setup/navSection/navSection.ts
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from "lwc";
-import type { NavData } from "../navItem/navItem";
-
-export default class NavSection extends LightningElement {
-    @api navItems: NavData[];
-    @api header?: string;
-}
